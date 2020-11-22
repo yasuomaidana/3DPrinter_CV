e5912929ac520b0bf3dbfe2b354fcea7163d54df
Yasuo Maidana <a01328427@itesm.mx>
2020-11-22 15:24:50 -0600

    new capabilities added

--- test_video.py
+++ test_video.py
@@ -1,10 +1,32 @@
+import filters
+
 import cv2
 import numpy as np
-
-##Miscelaneous
 import naviFiles as navFil
-#X:94.00 Y:-52.00 Z:159.00 E:0.00
+filters.creatFilterBarsWindow()
+def readHSV(param):
+    lH=param["min_H"]
+    lS=param["min_S"]
+    lV=param["min_V"]
+    uH=param["max_H"]
+    uS=param["max_S"]
+    uV=param["max_V"]
+    l_color = np.array([lH,lS,lV])
+    u_color = np.array([uH,uS,uV])
+    return l_color,u_color
 
+def HSV_Masked(frame,param):
+    if param["on"]==0:
+        return frame
+    else:
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        l_C,u_C=readHSV(param)
+        mask = cv2.inRange(hsv,l_C,u_C)
+        masked = cv2.bitwise_and(frame, frame, mask = mask)
+        return masked
+############################################################
+############################################################
+############################################################
 # function called by trackbar, sets the next frame to be read
 def getFrame(frame_nr):
     global video
@@ -29,96 +51,35 @@ playSpeed = 50
 cv2.createTrackbar("Frame", "Video", 0,nr_of_frames,getFrame)
 cv2.createTrackbar("Speed", "Video", playSpeed,100,setSpeed)
 
-#############Function to create HSV manipulator stuffs
-trName= "Track bars HSV format"
+############################################################
+############################################################
+############################################################
+processes={"hsv":HSV_Masked}
+def ordProcess(toWork,frame,param):
+    sendParam=param[toWork]
+    return processes[toWork](frame,sendParam)
 
-#Created to use Trackbar
-def nothing(x):
-    pass
-#Calibration calls bars values
-def cali():
-    # Obtain HSV values to create mask
-    l_h = cv2.getTrackbarPos("Low-H",trName)
-    l_s = cv2.getTrackbarPos("Low-S",trName)
-    l_v = cv2.getTrackbarPos("Low-V",trName)
-    u_h = cv2.getTrackbarPos("High-H",trName)
-    u_s = cv2.getTrackbarPos("High-S",trName)
-    u_v = cv2.getTrackbarPos("High-V",trName)
-
-    # Create levels arrays
-    l_color = np.array([l_h,l_s,l_v])
-    u_color = np.array([u_h,u_s,u_v])
-
-    #Obtain kernel size
-    ks = cv2.getTrackbarPos("KS","Kernel Size")
-
-    return [l_color, u_color,ks]
-
-#Creates bars to obtain HSV values
-def creatBar():
-    #### Create track bars
-    cv2.namedWindow(trName)
-    ## Low HSV level
-    cv2.createTrackbar("Low-H",trName,0,180,nothing)
-    cv2.createTrackbar("Low-S",trName,0,255,nothing)
-    cv2.createTrackbar("Low-V",trName,0,255,nothing)
-    ## High HSV level
-    cv2.createTrackbar("High-H",trName,180,180,nothing)
-    cv2.createTrackbar("High-S",trName,255,255,nothing)
-    cv2.createTrackbar("High-V",trName,255,255,nothing)
-    ### Create track bars end
-
-    ##Bar to get kernel
-    cv2.namedWindow("Kernel Size")
-    cv2.createTrackbar("KS","Kernel Size",0,255,nothing)
-###########end of HSV manipulator stuffs
-#Obtains the mask used for obtain the color cells
-def Obtain_Mask(frame):
-    #creatBar()
-    #while(True): 
-        # Capture frame-by-frame
-    #_, frame = cap.read()
-        #Convert mask to HSV format
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    #if cal:
-    [l_color, u_color,ks] = cali()
-    #else:
-        #[l_color, u_color,ks] = noCali()
-
-        #Create mask 
-    mask = cv2.inRange(hsv,l_color,u_color)
-
-        #Erode mask
-    kernel = np.ones((ks,ks),np.uint8)
-    mask = cv2.erode(mask,kernel)
-        #Dilatation
-    mask = cv2.dilate(mask, kernel)
-    
-    cv2.imshow('Mask',mask)
-# main loop
-creatBar()
 while 1:
-    # Get the next videoframe
     ret, frame = video.read()
+
+    
 
     # show frame, break the loop if no frame is found
     if ret:
         cv2.imshow("Video", frame)
+        param=filters.readBars()
+        
+        modi=ordProcess("hsv",frame,param)
+        cv2.imshow("Processed", modi)
         # update slider position on trackbar
         # NOTE: this is an expensive operation, remove to greatly increase max playback speed
         cv2.setTrackbarPos("Frame","Video", int(video.get(cv2.CAP_PROP_POS_FRAMES)))
     else:
         break
 
-    
-    Obtain_Mask(frame)
-    # display frame for 'playSpeed' ms, detect key input
     key = cv2.waitKey(playSpeed)
 
     # stop playback when q is pressed
     if key == ord('q'):
+        print(filters.readBars())
         break
-
-# release resources
-video.release()
-cv2.destroyAllWindows()
